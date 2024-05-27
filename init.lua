bf226ad5d742fd2f280d0d0fd09a7eb89d3394e9
Tony Liam Merrigan <Tony.Liam.Merrigan@gmail.com>
2024-05-27 15:33:34 +0100

    feat: java support

--- init.lua
+++ init.lua
@@ -152,7 +152,48 @@ require('lazy').setup({
     },
     build = ':TSUpdate',
   },
-
+  {
+    -- https://github.com/nvim-java/nvim-java
+    'nvim-java/nvim-java',
+    dependencies = {
+      'nvim-java/lua-async-await',
+      'nvim-java/nvim-java-refactor',
+      'nvim-java/nvim-java-core',
+      'nvim-java/nvim-java-test',
+      'nvim-java/nvim-java-dap',
+      'MunifTanjim/nui.nvim',
+      'neovim/nvim-lspconfig',
+      'mfussenegger/nvim-dap',
+      {
+        'williamboman/mason.nvim',
+        opts = {
+          registries = {
+            'github:nvim-java/mason-registry',
+            'github:mason-org/mason-registry',
+          },
+        },
+      }
+    },
+  },
+  {
+    "Zeioth/compiler.nvim",
+    cmd = {"CompilerOpen", "CompilerToggleResults", "CompilerRedo"},
+    dependencies = { "stevearc/overseer.nvim" },
+    opts = {},
+  },
+  { -- The task runner used by compiler.nvim
+    "stevearc/overseer.nvim",
+    commit = "68a2d344cea4a2e11acfb5690dc8ecd1a1ec0ce0",
+    cmd = { "CompilerOpen", "CompilerToggleResults", "CompilerRedo" },
+    opts = {
+      task_list = {
+        direction = "bottom",
+        min_height = 25,
+        max_height = 25,
+        default_detail = 1
+      },
+    },
+  },
   -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
   --       These are some example plugins that I've included in the kickstart repository.
   --       Uncomment any of the lines below to enable them.
@@ -241,6 +282,35 @@ vim.api.nvim_create_autocmd('FileType', {
     })
   end,
 })
+
+-- Function to check if a floating dialog exists and if not
+  -- then check for diagnostics under the cursor
+  function OpenDiagnosticIfNoFloat()
+    for _, winid in pairs(vim.api.nvim_tabpage_list_wins(0)) do
+      if vim.api.nvim_win_get_config(winid).zindex then
+        return
+      end
+    end
+    -- THIS IS FOR BUILTIN LSP
+    vim.diagnostic.open_float({0}, {
+      scope = "cursor",
+      focusable = false,
+      close_events = {
+        "CursorMoved",
+        "CursorMovedI",
+        "BufHidden",
+        "InsertCharPre",
+        "WinLeave",
+      },
+    })
+  end
+  -- Show diagnostics under the cursor when holding position
+  vim.api.nvim_create_augroup("lsp_diagnostics_hold", { clear = true })
+  vim.api.nvim_create_autocmd({ "CursorHold" }, {
+    pattern = "*",
+    command = "lua OpenDiagnosticIfNoFloat()",
+    group = "lsp_diagnostics_hold",
+  })
 -- [[ Configure Telescope ]]
 -- See `:help telescope` and `:help telescope.setup()`
 require('telescope').setup {
@@ -484,3 +554,5 @@ cmp.setup {
   },
 }
 require('whynotea')
+require('java').setup()
+require('lspconfig').jdtls.setup({})
