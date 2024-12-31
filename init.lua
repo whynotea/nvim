bf5180ccadfa26b7e84c62a8e074d2ecc24d5f14
Tony Liam Merrigan <Tony.Liam.Merrigan@gmail.com>
2024-12-31 11:04:57 +0000

    fix: remove java support due to long initial install time

--- init.lua
+++ init.lua
@@ -153,29 +153,6 @@ require('lazy').setup({
     build = ':TSUpdate',
   },
   {
-    -- https://github.com/nvim-java/nvim-java
-    'nvim-java/nvim-java',
-    dependencies = {
-      'nvim-java/lua-async-await',
-      'nvim-java/nvim-java-refactor',
-      'nvim-java/nvim-java-core',
-      'nvim-java/nvim-java-test',
-      'nvim-java/nvim-java-dap',
-      'MunifTanjim/nui.nvim',
-      'neovim/nvim-lspconfig',
-      'mfussenegger/nvim-dap',
-      {
-        'williamboman/mason.nvim',
-        opts = {
-          registries = {
-            'github:nvim-java/mason-registry',
-            'github:mason-org/mason-registry',
-          },
-        },
-      }
-    },
-  },
-  {
     "nvim-tree/nvim-tree.lua",
     version = "*",
     lazy = false,
@@ -569,5 +546,4 @@ cmp.setup {
   },
 }
 require('whynotea')
-require('java').setup()
 require('lspconfig').jdtls.setup({})
